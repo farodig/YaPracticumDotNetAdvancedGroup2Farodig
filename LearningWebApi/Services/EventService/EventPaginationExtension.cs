90a9e0323eb5d8bc18df973135a7e832100fa2b0
Максим <farodig@mail.ru>
2026-04-25 20:50:45 +0300

    Этап 5. Добавил проверку pagination нижнее граничное значение больше 0 т. е. не имеет смысла, верхнее значение не задано в требованиях. Дефолтные значения задаются в контроллере а не в сервисе, поэтому без проверки дефолтных значений в сервисе.

--- LearningWebApi/Services/EventService/EventPaginationExtension.cs
+++ LearningWebApi/Services/EventService/EventPaginationExtension.cs
@@ -10,6 +10,16 @@
         /// </summary>
         public static IEnumerable<Event> Pagination(this IEnumerable<Event> data, int page, int pageSize)
         {
+            if (page <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(page), $"The parameter must be positive and above zero");
+            }
+
+            if (pageSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageSize), $"The parameter must be positive and above zero");
+            }
+
             return data.OrderBy(c => c.StartAt)
                 .Skip((page - 1) * pageSize)
                 .Take(pageSize);
