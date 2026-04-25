52a8dbcb1681092e80c6cb300a21bab2923dbeb8
Максим <farodig@mail.ru>
2026-04-25 20:01:58 +0300

    Этап 5. Пока что Moq избыточен и не используется, поэтому отключаю и добавляю конструктор который сократит количество кода

---
+++ LearningTest/EventServiceFacade.cs
@@ -0,0 +1,25 @@
+﻿using LearningWebApi.Repositories;
+using LearningWebApi.Services.EventService;
+
+namespace LearningTest
+{
+    /// <summary>
+    /// Вспомогательный класс для инициализации репозитория
+    /// </summary>
+    public static class EventServiceFacade
+    {
+        /// <summary>
+        /// Создание сервиса событий с подготовленными данными для тестов
+        /// использование moq пока что супер избыточно (написание тестов ради тестов|кода ради кода)
+        /// </summary>
+        public static IEventService CreateEventService(params IEnumerable<Event> events)
+        {
+            var eventRepository = new EventRepository() as IEventRepository;
+            foreach (var item in events)
+            {
+                eventRepository.Add(item.Id, item);
+            }
+            return new EventService(eventRepository);
+        }
+    }
+}
