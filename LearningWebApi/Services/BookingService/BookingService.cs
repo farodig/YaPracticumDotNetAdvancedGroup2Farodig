cd0443e650826ebd7e419fbb6d4192dabe1fa72a
Максим <farodig@mail.ru>
2026-05-14 02:04:05 +0300

    Этап 4. Событие можно забронировать только один раз

--- LearningWebApi/Services/BookingService/BookingService.cs
+++ LearningWebApi/Services/BookingService/BookingService.cs
@@ -1,17 +1,27 @@
 ﻿using LearningWebApi.Entities;
 using LearningWebApi.Entities.Factories;
 using LearningWebApi.Repositories;
+using System.Collections.Concurrent;
 
 namespace LearningWebApi.Services.BookingService
 {
     internal class BookingService(IBookingRepository repository) : IBookingService
     {
         private IBookingRepository _repository = repository;
+        private readonly ConcurrentDictionary<Guid, Guid> _mapEventToBooking = new();
 
         public async ValueTask<Booking> CreateBookingAsync(Guid eventId)
         {
+            // одно событие можно забронировать только один раз
+            if (_mapEventToBooking.TryGetValue(eventId, out Guid bookingId)
+                && await GetBookingByIdAsync(bookingId) is Booking cached)
+            {
+                return cached;
+            }
+
             var booking = BookingFactory.CreateBooking(eventId);
             _repository.Add(booking.Id, booking);
+            _mapEventToBooking[eventId] = booking.Id;
             return booking;
         }
 
