940206da116a00fff9452402b542f32a0c3166f7
Максим <farodig@mail.ru>
2026-07-02 16:25:31 +0300

    Этап 4. Настройка DbContext

---
+++ LearningWebApi/DataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LearningWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningWebApi.DataAccess
+{
+    internal sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
