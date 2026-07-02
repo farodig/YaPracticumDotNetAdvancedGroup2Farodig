2eb3a3bb0eab0dd1151450b8b3457fc255c9dcee
Максим <farodig@mail.ru>
2026-07-02 17:21:34 +0300

    Этап 5. Описание маппинга через Fluent API - за исключением требования описывать связь одновременно в двух конфигурациях - по сути дублирование

---
+++ LearningWebApi/DataAccess/AppDbContextConfigurations/BookingConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using LearningWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningWebApi.DataAccess.AppDbContextConfigurations
+{
+    internal class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            // Таблица
+            builder.ToTable("bookings");
+
+            // Первичный ключ
+            builder.HasKey(a => a.Id);
+            builder.Property(p => p.Id)
+                .ValueGeneratedNever();
+
+            // Храним Status как строку
+            builder.Property(b => b.Status)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasConversion(v => v.ToString().ToLower(), v => Enum.Parse<BookingStatus>(v, true));
+
+            // Связь с Event
+            builder.HasOne(b => b.Event)
+                .WithMany(e => e.Bookings)
+                .HasForeignKey(b => b.EventId);
+        }
+    }
+}
---
+++ LearningWebApi/DataAccess/AppDbContextConfigurations/EventConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using LearningWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningWebApi.DataAccess.AppDbContextConfigurations
+{
+    internal class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            // Таблица
+            builder.ToTable("events", t =>
+            {
+                // Ограничения даты и времени
+                t.HasCheckConstraint("CK_Event_Dates", "[StartAt] < [EndAt]");
+                t.HasCheckConstraint("CK_Event_StartAt", "[StartAt] > GETDATE()");
+            });
+
+            // Первичный ключ
+            builder.HasKey(a => a.Id);
+            builder.Property(p => p.Id)
+                .ValueGeneratedNever();
+
+            // Ограничения свойств
+            builder.Property(e => e.Title)
+                   .IsRequired();
+
+            builder.Property(e => e.StartAt)
+                   .IsRequired();
+
+            builder.Property(e => e.EndAt)
+                   .IsRequired();
+
+            builder.Property(e => e.TotalSeats)
+                .IsRequired()
+                .HasAnnotation("MinValue", 1);
+        }
+    }
+}
