89d89ff18e72c6391d23ae99ffdfbedab108a4fc
Максим <farodig@mail.ru>
2026-07-03 21:44:32 +0300

    - переписал "быстрые" методы репозитория на те что будут корректно проверяться с юнит тестами с базой inmemory

--- LearningWebApi/Repositories/EventRepository.cs
+++ LearningWebApi/Repositories/EventRepository.cs
@@ -23,37 +23,22 @@ namespace LearningWebApi.Repositories
 
         public async Task<int> TryUpdateAsync(Event item, CancellationToken? cts = null)
         {
-            try
-            {
-                return await _dbContext.Events
-                    .Where(a => a.Id == item.Id)
-                    .ExecuteUpdateAsync(setters => setters
-                    .SetProperty(a => a.Title, item.Title)
-                    .SetProperty(a => a.Description, item.Description)
-                    .SetProperty(a => a.StartAt, item.StartAt)
-                    .SetProperty(a => a.EndAt, item.EndAt)
-                    .SetProperty(a => a.TotalSeats, item.TotalSeats)
-                    .SetProperty(a => a.AvailableSeats, item.AvailableSeats)
-                    , cts ?? CancellationToken.None);
-            }
-            finally
-            {
-                _dbContext.ChangeTracker.Clear();
-            }
+            var existing = await _dbContext.Events.FindAsync(item.Id);
+            if (existing == null) return 0;
+
+            _dbContext.Entry(existing).CurrentValues.SetValues(item);
+            return await _dbContext.SaveChangesAsync(cts ?? CancellationToken.None);
         }
 
         public async Task<int> TryRemoveAsync(Guid id, CancellationToken? cts = null)
         {
-            try
-            {
-                return await _dbContext.Events
-                    .Where(a => a.Id == id)
-                    .ExecuteDeleteAsync(cts ?? CancellationToken.None);
-            }
-            finally
-            {
-                _dbContext.ChangeTracker.Clear();
-            }
+            var toDelete = await _dbContext.Events
+                .Where(a => a.Id == id)
+                .ToArrayAsync(cts ?? CancellationToken.None);
+            if (toDelete.Length == 0) return 0;
+
+            _dbContext.Events.RemoveRange(toDelete);
+            return await _dbContext.SaveChangesAsync(cts ?? CancellationToken.None);
         }
     }
 }
