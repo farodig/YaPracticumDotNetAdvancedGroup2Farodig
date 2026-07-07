4da1688067090efd8785779b7e1668d81c5985c9
Maxim Chistyakov <farodig@mail.ru>
2026-07-07 16:57:50 +0300

    Этап 2. Подключение NuGet-пакетов - подключил пакеьы - создал отдельный проект Learning.integrationTests для интеграционных тестов и перенёс туда существующие тесты (после миграции уже неработающие)

---
+++ Learning.IntegrationTests/Helpers/EntityFactory.cs
@@ -0,0 +1,24 @@
+﻿using LearningWebApi.Entities;
+
+namespace LearningTest.Helpers
+{
+    internal static class EntityFactory
+    {
+        public static Event CreateEvent() => new()
+        {
+            Id = Guid.NewGuid(),
+        };
+
+        public static Event CreateEvent(Action<Event>? configure = null)
+        {
+            var eventData = CreateEvent();
+            configure?.Invoke(eventData);
+            return eventData;
+        }
+
+        public static Booking CreateBooking() => new()
+        {
+            Id = Guid.NewGuid(),
+        };
+    }
+}
