ab91d2c0d89b6686ec8cd2e40c3b9cbdcae9e570
Maxim Chistyakov <farodig@mail.ru>
2026-07-08 16:12:18 +0300

    Исправил валидацию поля в БД TotalSeats > 0

--- LearningWebApi/Migrations/20260708131141_CK_Event_TotalSeats.Designer.cs
+++ LearningWebApi/Migrations/20260708131141_CK_Event_TotalSeats.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LearningWebApi.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LearningWebApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260708131141_CK_Event_TotalSeats")]
+    partial class CK_Event_TotalSeats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
