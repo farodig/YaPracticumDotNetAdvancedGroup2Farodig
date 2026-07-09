0c62b65b40231542bd981a2f13731f05b68e43ba
Maxim Chistyakov <farodig@mail.ru>
2026-07-09 05:37:08 +0300

    Критические требования - доработал отсутствующий интеграционный тест 05. Обновление контекста без сохранения

--- Learning.IntegrationTests/RepositoriesTests/EventRepositoryCrudTests.cs
+++ Learning.IntegrationTests/RepositoriesTests/EventRepositoryCrudTests.cs
@@ -128,5 +128,61 @@ namespace Learning.IntegrationTests.RepositoriesTests
             var deletedCount = verifyContext.Events.Count(b => b.Id == eventId);
             Assert.Equal(0, deletedCount);
         }
+
+        [Fact(DisplayName = "05. Обновление контекста без сохранения")]
+        public async Task TryUpdateContextAsyncTest()
+        {
+            // Arrange
+            await using var context = CreateContext();
+            var eventId = Guid.NewGuid();
+            var @event1 = CreateEvent(eventId);
+            context.Events.Add(@event1);
+            await context.SaveChangesAsync();
+
+            var repository = CreateEventRepository(context);
+            var @event2 = new Event
+            {
+                Id = eventId,
+                Title = "Событие 2",
+                StartAt = DateTime.Now.AddHours(4),
+                EndAt = DateTime.Now.AddHours(5),
+                Description = "Описание",
+                TotalSeats = 6,
+                AvailableSeats = 4,
+            };
+
+            // Act (кладём обновлённое событие в кеш контекста)
+            Assert.True(await repository.TryUpdateContextAsync(@event2));
+
+            // Assert (объект в кеше другого контекста ещё не в базе)
+            await using var verifyContext = CreateContext();
+            var notUpdated = await verifyContext.Events.SingleAsync(b => b.Id == eventId);
+            Assert.NotEqual(@event2.Title, notUpdated.Title);
+            Assert.NotEqual(@event2.StartAt, notUpdated.StartAt);
+            Assert.NotEqual(@event2.EndAt, notUpdated.EndAt);
+            Assert.NotEqual(@event2.Description, notUpdated.Description);
+            Assert.NotEqual(@event2.TotalSeats, notUpdated.TotalSeats);
+            Assert.NotEqual(@event2.AvailableSeats, notUpdated.AvailableSeats);
+
+            // Act (сохраняем в базу)
+            await context.SaveChangesAsync();
+            verifyContext.ChangeTracker.Clear();
+
+            // Assert (теперь объект действительно сохранён - проверяем)
+            var updatedCollection = await verifyContext.Events
+                .Where(b => b.Id == eventId)
+                .ToListAsync();
+            Assert.Single(updatedCollection);
+
+            var updated = updatedCollection.FirstOrDefault();
+            Assert.NotNull(updated);
+            Assert.Equal(@event2.Id, updated.Id);
+            Assert.Equal(@event2.Title, updated.Title);
+            Assert.Equal(@event2.StartAt, updated.StartAt, TimeSpan.FromMilliseconds(1));
+            Assert.Equal(@event2.EndAt, updated.EndAt, TimeSpan.FromMilliseconds(1));
+            Assert.Equal(@event2.Description, updated.Description);
+            Assert.Equal(@event2.TotalSeats, updated.TotalSeats);
+            Assert.Equal(@event2.AvailableSeats, updated.AvailableSeats);
+        }
     }
 }
