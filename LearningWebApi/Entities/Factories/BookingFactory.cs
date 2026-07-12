cb32e46c99b829a9da94f58b3f933f90f944537a
Максим <farodig@mail.ru>
2026-07-13 02:56:28 +0300

    Этап 4. Перенос в Application - проект сломан, т.к. напрямую починить его без реализации следующих этапов нельзя

--- LearningWebApi/Entities/Factories/BookingFactory.cs
+++ LearningWebApi/Entities/Factories/BookingFactory.cs
@@ -5,14 +5,6 @@ namespace LearningWebApi.Entities.Factories
 {
     internal static class BookingFactory
     {
-        internal static Booking CreateBooking(Guid eventId) => new()
-        {
-            Id = Guid.NewGuid(),
-            EventId = eventId,
-            Status = Domain.Entities.BookingStatus.Pending,
-            CreatedAt = DateTime.Now,
-        };
-
         internal static BookingResponse ToBookingResponse(this Booking data) => new()
         {
             Id = data.Id,
