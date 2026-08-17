e9c7df022710cf0f530721740f59ea850f1d6cac
Максим <farodig@mail.ru>
2026-08-17 14:33:50 +0300

    добавил общий проект SharedContracts заготовку, для контрактов событий и заголовков для брокера

---
+++ Shared/SharedContracts/BrokerTopics/BookingTopics.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedContracts.BrokerTopics
+{
+    public static class BookingTopics
+    {
+        public const string BookingConfirmed = "booking-confirmed";
+    }
+}
---
+++ Shared/SharedContracts/Events/BookingConfirmedEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace SharedContracts.Events
+{
+    /// <summary>
+    /// Событие бронирование подтверждено
+    /// </summary>
+    public sealed record BookingConfirmedEvent
+    {
+        /// <summary>
+        /// Идентификатор бронирования
+        /// </summary>
+        public Guid BookingId { get; set; }
+
+        /// <summary>
+        /// Идентификатор события
+        /// </summary>
+        public Guid EventId { get; set; }
+        
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public Guid PersonId { get; set; }
+        
+        /// <summary>
+        /// Количество забронированных мест
+        /// </summary>
+        public int SeatsCount { get; set; }
+        
+        /// <summary>
+        /// Время подтверждения брони
+        /// </summary>
+        public DateTime ConfirmedAt { get; set; }
+    }
+}
